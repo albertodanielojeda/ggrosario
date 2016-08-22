8cff61dad0bfb8d52276230a8949ea6bec67d727
Alberto D. Ojeda <albertoojeda@live.com.ar>
2016-08-22 11:28:26 -0300

    Added class ListaDeseosJuegos
    This class serves as cross reference between Juego and ListaDeseos
    classes
    - hibernate.cfg.xml adds its mapping
    - Juego and ListaDeseos class have a one-to-many relationship with the
    cross reference.

--- src/main/java/com/herokuapp/ggrosario/modelo/ListaDeseos.java
+++ src/main/java/com/herokuapp/ggrosario/modelo/ListaDeseos.java
@@ -8,61 +8,78 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import com.herokuapp.ggrosario.util.HibernateUtil;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author Ojeda Alberto Daniel
  */
 @Entity
-@Table(name="lista_de_deseos")
+@Table(name = "lista_de_deseos")
 public class ListaDeseos implements Serializable {
-    
-    // <editor-fold defaultstate="collapsed" desc="Class attributes. Click on the + sign on the left to edit the code.">
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="id") private int id;
-    
-    @OneToMany
-    private List<Juego> juegos;
-    
+    @Column(name = "id")
+    private int id;
+
+    @OneToMany(mappedBy = "unaListaDeseos")
+    private List<ListaDeseosJuegos> unaListaDeseosJuegos;
+
     @OneToOne(mappedBy = "unaListaDeseos")
     private Usuario unUsuario;
-    // </editor-fold>
 
     /**
      * Constructor nulo para inicializar las colecciones
      */
     public ListaDeseos() {
-        this.juegos = new ArrayList<>();
+        this.unaListaDeseosJuegos = new ArrayList<>();
     }
 
     /**
      * Constructor para instanciar una lista de deseos de un usuario
-     * @param unUsuario Instancia de la clase Usuario a la que se le asignara
-     * la lista de deseos
+     *
+     * @param unUsuario Instancia de la clase Usuario a la que se le asignara la
+     * lista de deseos
      */
     public ListaDeseos(Usuario unUsuario) {
         this();
         this.unUsuario = unUsuario;
         HibernateUtil.guardar(this);
     }
-    
+
     /**
      * Agrega un juego a esta lista de deseos
-     * @param unJuego Instancia de la clase Juego que se agregará a esta 
-     * lista de deseos
+     *
+     * @param unJuego Instancia de la clase Juego que se agregará a esta lista
+     * de deseos
      */
-    public void addJuego(Juego unJuego){
-        this.juegos.add(unJuego);
-        HibernateUtil.actualizar(this);
+    public void addJuego(Juego unJuego) {
+        if (!existeJuego(unJuego)) {
+            ListaDeseosJuegos listaDeseosJuegos = new ListaDeseosJuegos(this, unJuego);
+            this.unaListaDeseosJuegos.add(listaDeseosJuegos);
+            HibernateUtil.actualizar(this);
+        }
     }
+    // No funciona como se espera
+    public boolean existeJuego(Juego unJuego) {
+        for (ListaDeseosJuegos listaDeseosJuegos : this.unaListaDeseosJuegos) {
+            if (listaDeseosJuegos.getUnJuego() == unJuego) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
 
     // <editor-fold defaultstate="collapsed" desc="Getters and setters methods. Click on the + sign on the left to edit the code.">
-    
     public int getId() {
         return id;
     }
@@ -71,14 +88,14 @@ public class ListaDeseos implements Serializable {
         this.id = id;
     }
 
-    public List<Juego> getJuegos() {
-        return juegos;
+    public List<ListaDeseosJuegos> getUnaListaDeseosJuegos() {
+        return unaListaDeseosJuegos;
     }
 
-    public void setJuegos(List<Juego> juegos) {
-        this.juegos = juegos;
+    public void setUnaListaDeseosJuegos(List<ListaDeseosJuegos> unaListaDeseosJuegos) {
+        this.unaListaDeseosJuegos = unaListaDeseosJuegos;
     }
-    
+
     public Usuario getUnUsuario() {
         return unUsuario;
     }
@@ -86,8 +103,6 @@ public class ListaDeseos implements Serializable {
     public void setUnUsuario(Usuario unUsuario) {
         this.unUsuario = unUsuario;
     }
-    
+
     // </editor-fold>
-    
-    
 }
