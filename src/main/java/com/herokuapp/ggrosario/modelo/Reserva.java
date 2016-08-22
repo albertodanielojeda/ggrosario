6155de4c0519a85f7c737c13ba1863b7e7e5d7c7
Alberto D. Ojeda <albertoojeda@live.com.ar>
2016-08-21 21:40:20 -0300

    Added class Reserva

---
+++ src/main/java/com/herokuapp/ggrosario/modelo/Reserva.java
@@ -0,0 +1,115 @@
+package com.herokuapp.ggrosario.modelo;
+
+import com.herokuapp.ggrosario.util.HibernateUtil;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Ojeda Alberto Daniel
+ */
+@Entity
+@Table(name = "Reservas")
+public class Reserva implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    private Usuario unUsuario;
+
+    @ManyToOne
+    private Juego unJuego;
+
+    @Column(name = "fecha_alta")
+    private GregorianCalendar fechaAlta;
+
+    @Column(name = "fecha_baja")
+    private GregorianCalendar fechaBaja;
+
+    /**
+     * Constructor nulo
+     */
+    public Reserva() {
+    }
+
+    /**
+     * Constructor para instanciar una reserva
+     *
+     * @param unUsuario Usuario que hace la reserva
+     * @param unJuego Juego que reserva el usuario
+     */
+    public Reserva(Usuario unUsuario, Juego unJuego) {
+        this.unUsuario = unUsuario;
+        this.unJuego = unJuego;
+        this.fechaAlta = new GregorianCalendar();
+        // falta this.fechaBaja
+        HibernateUtil.guardar(this);
+    }
+
+    /**
+     * Verifica si el juego que la reserva tiene asociado es el mismo que el que
+     * se le envia
+     *
+     * @param unJuego Juego a comparar con el de la reserva
+     * @return <code>true</code> si el juego enviado como parámetro es el mismo
+     * que el que contiene la reserva, en caso contrario devuelve
+     * <code>false</code>
+     */
+    public boolean isJuego(Juego unJuego) {
+        return this.unJuego.equals(unJuego);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Getters and setters methods. Click on the + sign on the left to edit the code.">
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUnUsuario() {
+        return unUsuario;
+    }
+
+    public void setUnUsuario(Usuario unUsuario) {
+        this.unUsuario = unUsuario;
+    }
+
+    public Juego getUnJuego() {
+        return unJuego;
+    }
+
+    public void setUnJuego(Juego unJuego) {
+        this.unJuego = unJuego;
+    }
+
+    public GregorianCalendar getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(GregorianCalendar fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public GregorianCalendar getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(GregorianCalendar fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+    // <editor-fold>
+}
