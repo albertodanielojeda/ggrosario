3cf0f75fe8e65df4febb7b44da2176a703d91650
Alberto D. Ojeda <albertoojeda@live.com.ar>
2016-08-25 18:14:33 -0300

    New UnitTests
    TiendaTest.java es una unidad de pruebas que por ahora no ofrece nada.
    UsuarioComentarioTest.java es una unidad de prueba que testea el acceso
    de varios usuarios sobre (posiblemente) el mismo juego para dejar un
    comentario. Los usuarios y los juegos son seleccionados aleatoriamente.

---
+++ src/test/java/com/herokuapp/ggrosario/modelo/TiendaTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.herokuapp.ggrosario.modelo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ojeda Alberto Daniel
+ */
+public class TiendaTest {
+    
+    public TiendaTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        Tienda.getInstance().ordenarJuegosMasReservados();
+    }
+    
+}
