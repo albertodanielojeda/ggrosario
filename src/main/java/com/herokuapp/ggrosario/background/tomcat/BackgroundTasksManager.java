cc83d5ea9265f609401bf147a087a2d27021c336
Alberto D. Ojeda <albertoojeda@live.com.ar>
2016-10-09 00:52:08 -0300

    Modificación al planificador para Tomcat

--- src/main/java/com/herokuapp/ggrosario/background/tomcat/BackgroundTasksManager.java
+++ src/main/java/com/herokuapp/ggrosario/background/tomcat/BackgroundTasksManager.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.herokuapp.ggrosario.background.tomcat;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 /**
  *
  * @author Ojeda Alberto Daniel
  */
-public class BackgroundTasksManager {
+@WebListener
+public class BackgroundTasksManager implements ServletContextListener{
+
+    private ScheduledExecutorService planificador;
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        this.planificador = Executors.newSingleThreadScheduledExecutor();
+        this.planificador.scheduleAtFixedRate(new ReservasChecker(), 0, 1, TimeUnit.DAYS);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        this.planificador.shutdownNow();
+    }
     
 }
