8447d71a8e84ab9404bb8bea685909b558a755ec
albertodanielojeda <albertoojeda@live.com.ar>
2016-10-21 01:40:11 -0300

    Class ReservasChecker for Tomcat wasn't public

--- src/main/java/com/herokuapp/ggrosario/background/tomcat/ReservasChecker.java
+++ src/main/java/com/herokuapp/ggrosario/background/tomcat/ReservasChecker.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.herokuapp.ggrosario.background.tomcat;
 
 import com.herokuapp.ggrosario.modelo.Reserva;
@@ -15,7 +10,7 @@ import java.util.List;
  *
  * @author Ojeda Alberto Daniel
  */
-class ReservasChecker implements Runnable {
+public class ReservasChecker implements Runnable {
 
     @Override
     public void run() {
